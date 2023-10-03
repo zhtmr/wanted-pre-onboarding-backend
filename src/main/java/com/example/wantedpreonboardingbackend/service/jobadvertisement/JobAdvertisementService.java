c48922c63377f48f42575eceaf6a335fc4616fbb
zhtmr <zhtmr123@gmail.com>
2023-10-03 15:14:39 +0900

    Feat: 채용공고 crud api 추가

--- src/main/java/com/example/wantedpreonboardingbackend/service/jobadvertisement/JobAdvertisementService.java
+++ src/main/java/com/example/wantedpreonboardingbackend/service/jobadvertisement/JobAdvertisementService.java
@@ -1,18 +1,64 @@
 package com.example.wantedpreonboardingbackend.service.jobadvertisement;
 
+import com.example.wantedpreonboardingbackend.domain.company.Company;
+import com.example.wantedpreonboardingbackend.domain.company.CompanyRepository;
+import com.example.wantedpreonboardingbackend.domain.jobadvertisement.JobAdvertisement;
 import com.example.wantedpreonboardingbackend.domain.jobadvertisement.JobAdvertisementRepository;
+import com.example.wantedpreonboardingbackend.domain.user.UserRepository;
+import com.example.wantedpreonboardingbackend.web.dto.JobAdvertisementDeleteDto;
+import com.example.wantedpreonboardingbackend.web.dto.JobAdvertisementResponseDto;
 import com.example.wantedpreonboardingbackend.web.dto.JobAdvertisementSaveRequestDto;
+import com.example.wantedpreonboardingbackend.web.dto.JobAdvertisementUpdateRequestDto;
+import com.example.wantedpreonboardingbackend.web.message.Result;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 public class JobAdvertisementService {
     private final JobAdvertisementRepository jobAdvertisementRepository;
+    private final CompanyRepository companyRepository;
 
+    // 등록
     @Transactional
-    public Long save(JobAdvertisementSaveRequestDto requestDto) {
-        return jobAdvertisementRepository.save(requestDto.toEntity()).get채용공고_id();
+    public Result<?> save(Long companyId, JobAdvertisementSaveRequestDto requestDto) {
+        Company company = companyRepository.findById(companyId).orElseThrow(() -> new IllegalArgumentException("해당 회사가 없습니다. companyId = " + companyId));
+        JobAdvertisement saved = jobAdvertisementRepository.save(requestDto.toEntity(company));
+        return new Result<>(new JobAdvertisementResponseDto(saved));
     }
+
+    // 수정
+    @Transactional
+    public Result<?> update(Long companyId, Long jobAdvertisementId, JobAdvertisementUpdateRequestDto requestDto) {
+        Company company = companyRepository.findById(companyId).orElseThrow(() -> new IllegalArgumentException("해당 회사가 없습니다. companyId = " + companyId));
+        JobAdvertisement jobAdvertisement = jobAdvertisementRepository.findById(jobAdvertisementId).orElseThrow(() -> new IllegalArgumentException("해당 채용 공고가 없습니다. jobAdvertisementId = " + jobAdvertisementId));
+        jobAdvertisement.update(requestDto, company);
+        return new Result<>(new JobAdvertisementResponseDto(jobAdvertisement));
+    }
+
+    // 삭제
+    @Transactional
+    public Result<?> delete(JobAdvertisementDeleteDto deleteDto) {
+        JobAdvertisement entity = jobAdvertisementRepository.findById(deleteDto.getJob_advertisement_id()).orElseThrow(() -> new IllegalArgumentException("해당 채용 공고가 없습니다. jobAdvertisementId = " + deleteDto.getJob_advertisement_id()));
+        jobAdvertisementRepository.delete(entity);
+        return new Result<>().setMessage("채용공고가 삭제되었습니다.");
+    }
+
+
+    // 전체 조회
+    @Transactional
+    public Result<?> findAllDesc() {
+        List<JobAdvertisementResponseDto> list = jobAdvertisementRepository.findAllDesc().stream()
+                .map(JobAdvertisementResponseDto::new)
+                .collect(Collectors.toList());
+        return new Result<>(list);
+    }
+
+
+
 }
--- src/main/java/com/example/wantedpreonboardingbackend/web/JobAdvertisementApiController.java
+++ src/main/java/com/example/wantedpreonboardingbackend/web/JobAdvertisementApiController.java
@@ -1,20 +1,53 @@
 package com.example.wantedpreonboardingbackend.web;
 
 import com.example.wantedpreonboardingbackend.service.jobadvertisement.JobAdvertisementService;
+import com.example.wantedpreonboardingbackend.web.dto.JobAdvertisementDeleteDto;
+import com.example.wantedpreonboardingbackend.web.dto.JobAdvertisementResponseDto;
 import com.example.wantedpreonboardingbackend.web.dto.JobAdvertisementSaveRequestDto;
+import com.example.wantedpreonboardingbackend.web.dto.JobAdvertisementUpdateRequestDto;
+import com.example.wantedpreonboardingbackend.web.message.Result;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/api/v1/job-advertisements")
 public class JobAdvertisementApiController {
 
     private final JobAdvertisementService jobAdvertisementService;
 
-    @PostMapping("/api/v1/job-advertisements")
-    public Long save(@RequestBody JobAdvertisementSaveRequestDto requestDto) {
-        return jobAdvertisementService.save(requestDto);
+    // 등록
+    @PostMapping("/{companyId}")
+    public ResponseEntity<?> save(@PathVariable Long companyId, @RequestBody JobAdvertisementSaveRequestDto requestDto) {
+        Result<?> result = jobAdvertisementService.save(companyId, requestDto);
+        return new ResponseEntity<Result<?>>(result, HttpStatus.OK);
     }
+
+    // 수정
+    @PutMapping("/{companyId}/{jobAdvertisementId}")
+    public ResponseEntity<?> update(@PathVariable Long companyId, @PathVariable Long jobAdvertisementId, @RequestBody JobAdvertisementUpdateRequestDto requestDto) {
+        Result<?> result = jobAdvertisementService.update(companyId, jobAdvertisementId, requestDto);
+        return new ResponseEntity<Result<?>>(result, HttpStatus.OK);
+    }
+
+    // 삭제
+    @DeleteMapping
+    public ResponseEntity<?> delete(@RequestBody JobAdvertisementDeleteDto deleteDto) {
+        Result<?> result = jobAdvertisementService.delete(deleteDto);
+        return new ResponseEntity<Result<?>>(result, HttpStatus.OK);
+    }
+
+    // 조회
+    @GetMapping
+    public ResponseEntity<?> selectAll() {
+        Result<?> result = jobAdvertisementService.findAllDesc();
+        return new ResponseEntity<Result<?>>(result, HttpStatus.OK);
+    }
+
+
 }
